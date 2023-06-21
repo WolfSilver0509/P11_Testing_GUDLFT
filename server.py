d6258a0d776ca7ae23c4638ed39663f7df73d9ec
Mydmoov <denizot.myriam@gmail.com>
2023-06-21 13:44:41 +0200

    Réstrindre les réservation pour les compétitions passée

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask,render_template,request,redirect,flash,url_for
-
+import datetime
 
 def loadClubs():
     with open('clubs.json') as c:
@@ -31,14 +31,25 @@ def showSummary():
 
 
 @app.route('/book/<competition>/<club>')
-def book(competition,club):
+def book(competition, club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
+
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        current_date = datetime.date.today()
+        competition_date = datetime.datetime.strptime(foundCompetition['date'], "%Y-%m-%d").date()
+
+        if competition_date >= current_date:
+            if foundCompetition['numberOfPlaces'] <= 12:
+                return render_template('booking.html', club=foundClub, competition=foundCompetition)
+            else:
+                flash("Vous ne pouvez réserver que 12 places maximum pour ce concours. / You can only book up to 12 places for this competition.")
+        else:
+            flash("Ce concours a déjà eu lieu. / This competition has already taken place.")
     else:
-        flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        flash("Une erreur s'est produite. Veuillez réessayer / Something went wrong - please try again")
+
+    return render_template('welcome.html', club=club, competitions=competitions)
 
 
 @app.route('/purchasePlaces',methods=['POST'])
