6507edd91eaee2144a01a934458edad9a96b8c61
Mydmoov <denizot.myriam@gmail.com>
2023-06-29 11:20:58 +0200

    update teste unitaire OK

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from server import app, clubs, competitions
+import json
+
+# Fixture pour les données de test
+@pytest.fixture
+def load_clubs():
+    with open('clubs.json') as c:
+        listOfClubs = json.load(c)['clubs']
+        return listOfClubs
+
+@pytest.fixture
+def load_competitions():
+    with open('competitions.json') as comps:
+        listOfCompetitions = json.load(comps)['competitions']
+        return listOfCompetitions
