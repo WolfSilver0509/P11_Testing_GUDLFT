910b56d017c5e8b2f88ddbec6ef094079817a3ee
Mydmoov <denizot.myriam@gmail.com>
2023-07-03 15:16:32 +0200

    up

--- locustfile.py
+++ locustfile.py
@@ -4,5 +4,5 @@ from locust import HttpUser, task
 class ProjectPerfTest(HttpUser):
     @task
     def home(self):
-        response = self.client.get("/index")
+        response = self.client.get("/")
 
